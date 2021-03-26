47838f9253bf943f950d5a6f9cc302f7aadc212b
realMidoss <74860384+realMidoss@users.noreply.github.com>
2021-03-26 21:09:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -389,7 +389,82 @@ async def kick(ctx,user:discord.Member = None):
       
 #Ekonomi Komutları 
 
+amounts = {}
 
+@bot.event
+async def on_ready():
+    global amounts
+    try:
+        with open('amounts.json') as f:
+            amounts = json.load(f)
+    except FileNotFoundError:
+        print("Could not load amounts.json")
+        amounts = {}
+
+@bot.command(pass_context=True)
+async def balance(ctx):
+    id = str(ctx.message.author.id)
+    if id in amounts:
+        await ctx.send("You have {} beans on your account!".format(amounts[id]))
+    else:
+        await ctx.send("You do not have an account use register command in order to create one!")
+
+@bot.command(pass_context=True)
+async def register(ctx):
+    id = str(ctx.message.author.id)
+    if id not in amounts:
+        amounts[id] = 100
+        await ctx.send("You are now registered")
+        _save()
+    else:
+        await ctx.send("You already have an account")
+
+@bot.command(pass_context=True)
+async def transfer(ctx, amount: int, other: discord.Member):
+    primary_id = str(ctx.message.author.id)
+    other_id = str(other.id)
+    if primary_id not in amounts:
+        await ctx.send("You do not have an account")
+    elif other_id not in amounts:
+        await ctx.send("The other party does not have an account")
+    elif amounts[primary_id] < amount:
+        await ctx.send("You cannot afford this transaction")
+    else:
+        amounts[primary_id] -= amount
+        amounts[other_id] += amount
+        await ctx.send("Transaction complete")
+    _save()
+
+def _save():
+    with open('amounts.json', 'w+') as f:
+        json.dump(amounts, f)
+    
+
+@bot.command()
+async def save():
+    _save()
+
+@bot.command()
+async def work(ctx):
+
+    workv = [
+    5,
+    10,
+    12,
+    15,
+    20,
+    35,
+    40,
+    70 
+    ]
+
+    id = str(ctx.message.author.id)
+    if id not in amounts:
+        await ctx.send("You do not have an account. So we can not pay u")   
+    else:
+        amounts[id] += random.choice(workv)
+        await ctx.send(f"You worked hard and got some beans")
+    _save()
 
 #help command
 
