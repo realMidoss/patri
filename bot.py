f6142bd79ed9a2a4bf7a74b2624e476eafd5124f
realMidoss <74860384+realMidoss@users.noreply.github.com>
2021-05-18 17:26:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -440,8 +440,8 @@ async def help(ctx):
     embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/762217890111029268.png?v=1")
     
     embed.add_field(name="help", value="you used it already, didnt ya?", inline=False)
-    embed.add_field(name="Economy Commands", value="balance, register, save, transfer, work")
-    embed.add_field(name="Fun Commands", value="ara, bruh, bully, declarecommunism, F, hug, invade, kick, kill, kiss, lap, marry, nuke, pat, say, suck, tsun, warn, question")
+    embed.add_field(name="Economy Commands", value="balance, register, save, toss, transfer, work")
+    embed.add_field(name="Fun Commands", value="ara, bruh, bully, declarecommunism, F, hug, invade, kick, kill, kiss, lap, marry, nuke, pat, say, suck, warn, question")
     embed.add_field(name="Usefull Commands", value="pfp, ping, poll, info, vote")
     
     await ctx.send(embed=embed)
