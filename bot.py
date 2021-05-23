2b1782ca108d34892f2a359194545aafab9c69c9
Jaymo <jaymo.averay@gmail.com>
2021-05-23 18:53:35 +0930

    feat: make bean economy persistent and its own cog
    Beans should now persist and save to a PostgreSQL database specified in
    the DATABASE_URL environment variable.
    
    Additionally, the bot token will be grabbed from PATRI_DISCORD_TOKEN if
    not specified on the command line.
    
    Some other small stuff was also changed like coin toss now being a
    consistant gamble instead of a random amount based on your initial bet.

--- bot.py
+++ bot.py
@@ -2,21 +2,28 @@ import discord
 from discord.ext import commands
 import random
 import asyncio
-import json
 import sys
+import os
 from cogs.Turkish import TRCog
 from cogs.sub import subCog
 from cogs.em import emCog
+from cogs.economy import BeansEconomyCog
+
+# From Heroku's own docs
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL is None:
+    DATABASE_URL = "postgres://postgres:password@127.0.0.1:5432/patridb"
 
 bot = commands.Bot(command_prefix=["patri ", "Patri ","p ","P "], help_command=None, allowed_mentions=discord.AllowedMentions(roles=False, users=False, everyone=False))
-
 bot.add_cog(TRCog(bot))
 bot.add_cog(subCog(bot))
 bot.add_cog(emCog(bot))
-
+bot.add_cog(BeansEconomyCog(bot, DATABASE_URL))
 
 @bot.event
 async def on_ready():
+    print("Bot looks to be connected to Discord!")
     game = discord.Game("Patri help")    
     await bot.change_presence(status=discord.Status.idle, activity=game)
 
@@ -100,184 +107,15 @@ async def help(ctx):
     embed.add_field(name="Usefull Commands", value="pfp, ping, poll, info, vote")
     
     await ctx.send(embed=embed)
-    
-#Ekonomi
 
-amounts = {}
+# Run the bot with a token specified via the command line or at the environment variable PATRI_DISCORD_TOKEN.
+if len(sys.argv) > 1:
+    os.environ["PATRI_DISCORD_TOKEN"] = str(sys.argv[1])
 
-@bot.event
-async def on_ready():
-    global amounts
-    try:
-        with open('amounts.json') as f:
-            amounts = json.load(f)
-    except FileNotFoundError:
-        print("Could not load amounts.json")
-        amounts = {}
+bot_token = os.environ.get("PATRI_DISCORD_TOKEN")
 
-@bot.command(pass_context=True)
-async def balance(ctx):
-    id = str(ctx.message.author.id)
-    if id in amounts:
-        await ctx.send("You have {} beans in the bank".format(amounts[id]))
-    else:
-        await ctx.send("You do not have an account. Create one with register command")
-
-@bot.command(pass_context=True)
-async def register(ctx):
-    id = str(ctx.message.author.id)
-    if id not in amounts:
-        amounts[id] = 100
-        await ctx.send("You are now registered")
-        _save()
-    else:
-        await ctx.send("You already have an account")
-
-@bot.command(pass_context=True)
-async def transfer(ctx, amount: int, other: discord.Member):
-    primary_id = str(ctx.message.author.id)
-    other_id = str(other.id)
-    if primary_id not in amounts:
-        await ctx.send("You do not have an account")
-    elif other_id not in amounts:
-        await ctx.send("The other party does not have an account")
-    elif amounts[primary_id] < amount:
-        await ctx.send("You cannot afford this transaction")
-    else:
-        amounts[primary_id] -= amount
-        amounts[other_id] += amount
-        await ctx.send("Transaction complete")
-    _save()
-
-def _save():
-    with open('amounts.json', 'w') as f:
-        json.dump(amounts, f)
-
-@bot.command()
-async def save():
-    _save()
-
-@bot.command()
-@commands.cooldown(1, 3600, commands.BucketType.user)
-async def work(ctx):
-
-    earned = random.randint(50, 250) 
-
-    id = str(ctx.message.author.id)
-    if id not in amounts:
-        await ctx.send("You do not have an account. So we can not pay u")   
-    else:
-        amounts[id] += earned 
-        
-        embed=discord.Embed(title="Work Work Work!", description=f"You worked hard and got {earned} beans", color = ctx.author.color)
-        embed.set_thumbnail(url="https://freepikpsd.com/wp-content/uploads/2019/10/tin-of-beans-png-transparent-tin-of-beanspng-images-pluspng-png-baked-beans-650_650.png")
-
-        await ctx.send(embed=embed)
-
-    pass
-
-    if id not in amounts:
-        work.reset_cooldown(ctx)
-
-    _save()
-@work.error
-async def work_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        msg = "You can only work once every hour \n Next work will be avaible in: {:.2f}s".format(error.retry_after)
-        
-        embed=discord.Embed(title="Bro chill!", description=f"{msg}", color = ctx.author.color)    
-        embed.set_thumbnail(url="http://cdn.onlinewebfonts.com/svg/img_571830.png")
-        await ctx.send(embed=embed)
-    else:
-        raise error
-
-@bot.command()
-@commands.cooldown(5, 180, commands.BucketType.user)
-async def toss(ctx, amount: int = None):
-
-    if amount == None:
-        await ctx.send("You have to give some beans before playing gamble")
-        return
-    
-    A = amount
-    user_id = str(ctx.message.author.id)
-    coin = random.randint(-2*A, 2*A)
-    
-    if user_id not in amounts:
-        await ctx.send("You do not have an account. So you can not play")   
-        return
-    elif amount > amounts[user_id]:
-        await ctx.send("You don't have enough beans")
-        return
-
-    else:
-        
-        amounts[user_id] += coin
-
-    if coin > 0:  
-        embed=discord.Embed(title="heads or tails!", description=f"You are lucky! You just got {coin} beans", color = ctx.author.color)
-        embed.set_thumbnail(url="https://www.pngrepo.com/download/261646/coin-flip.png")
-        await ctx.send(embed=embed)
-    elif coin <= 0:
-        em=discord.Embed(title="heads or tails!", description=f"Ah unlucky... You lost {coin} beans", color = ctx.author.color)
-        em.set_thumbnail(url="https://www.pngrepo.com/download/261646/coin-flip.png")
-        await ctx.send(embed=em)
-
-    def _save():
-        with open('amounts.json', 'w') as f:
-            json.dump(amounts, f)
-
-    _save()
-
-@toss.error
-async def toss_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        msg = "Wow You are doing that so often! \n Wait for: {:.2f}s".format(error.retry_after)
-        
-        embed=discord.Embed(title="Wait for a second!!", description=f"{msg}", color = ctx.author.color)    
-        embed.set_thumbnail(url="http://cdn.onlinewebfonts.com/svg/img_571830.png")
-        await ctx.send(embed=embed)
-    else:
-        raise error
-
-@bot.command()
-async def shop(ctx):
-    
-    shop = [
-        {"name":"GAM Mug","price":100,"description":"A Mug for hot ones"},
-        {"name":"OC Figure of Mods","price":250,"description":"Show your love to best mods"},
-        {"name":"Body Pillow","price":500,"description":"For loners/cultured ones"}]   
-    em = discord.Embed(title = "Shop")
-
-    for item in shop:
-        name = item["name"]
-        price = item["price"]
-        desc = item["description"]
-        em.add_field(name = name, value = f"{price} BEANS | {desc}")
-
-    await ctx.send(embed = em)
-    
-@bot.command()
-async def add(ctx, amount: int, user: discord.Member):
-
-    ID = 354316211657506827
-    user_id = str(user.id)
-    
-    if ctx.message.author.id == ID:
-        amounts[user_id] += amount
-        await ctx.send("Transaction complete")
-    else:
-        await ctx.send('You are not allowed to execute this command!')
-    
-    def _save():
-        with open('amounts.json', 'w') as f:
-            json.dump(amounts, f)
-
-    _save()
-
-# Run the bot with a token specified via the commandline (perhaps github project secrets could be used to hide the token too?)
-if len(sys.argv) < 2:
+if bot_token is None:
     print("Missing a bot token! Please specify a bot token as the first command line argument.")
 else:
-    token = str(sys.argv[1])
-    bot.run(token)
+    print("Attempting to start the bot...")
+    bot.run(bot_token)
