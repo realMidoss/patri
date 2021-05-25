a4bed1d2ce1aee05ae31c1febe33d5f6c8ea2ef0
realMidoss <74860384+realMidoss@users.noreply.github.com>
2021-05-25 12:37:12 +0300

    daily command

--- bot.py
+++ bot.py
@@ -94,6 +94,7 @@ async def HeroFighte(ctx):
     await ctx.send(embed=embed)
 
 #help command
+
 @bot.group(invoke_without_command=True)
 async def help(ctx):
 
@@ -101,11 +102,9 @@ async def help(ctx):
     embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/762217890111029268.png?v=1")
     
     embed.add_field(name="help", value="you used it already, didnt ya?", inline=False)
-    embed.add_field(name="Economy Commands", value="balance, steal, shop, toss, transfer, work")
+    embed.add_field(name="Economy Commands", value="balance, daily, steal, shop, toss, transfer, work")
     embed.add_field(name="Fun Commands", value="ara, bruh, bonk, bully, declarecommunism, F, hug, invade, kick, kill, kiss, lap, marry, nuke, pat, say, suck, warn, question")
     embed.add_field(name="Useful Commands", value="pfp, ping, poll, info, vote")
-    
-    await ctx.send(embed=embed)
 
 # Run the bot with a token specified via the command line or at the environment variable PATRI_DISCORD_TOKEN.
 if len(sys.argv) > 1:
